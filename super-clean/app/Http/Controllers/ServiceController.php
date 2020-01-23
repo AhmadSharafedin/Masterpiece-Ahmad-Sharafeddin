a34b771b856b73d3d5ee73fea516e31995cb4174
AhmadSharafedin <sharafedin.ahmad@gmail.com>
2020-01-23 12:26:51 +0200

    Update for implemtation source code

---
+++ super-clean/app/Http/Controllers/ServiceController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        $user_id = Auth::id();
+        $filter = 'All status';
+        $quaries = Service::where('user_id', $user_id)->with('user')->orderBy('created_at', 'desc')->get();
+        return view('pages.user_dashbord', compact('quaries','filter'));
+    }
+
+    public function create()
+    {
+//        return redirect()->route('service.index');
+    }
+
+    public function store(){
+
+
+
+        $service = new Service();
+        $service->service_category = request('service_category');
+        $service->service_type = request('service_type');
+        $service->city = request('city');
+        $service->no_of_hours = request('no_of_hours','1');
+        $service->no_of_workers = request('no_of_workers','1');
+        $service->price = request('price');
+        $service->address = request('address');
+        $service->description = request('description', '');
+        $service->date = request('date');
+        $service->user_id = Auth::id();
+        $service->save();
+        return redirect()->route('service.index');
+
+    }
+
+    public function show($status)
+    {
+        return $status;
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function filter(Request $request)
+    {
+        $user_id = Auth::id();
+        $filter = $request->input("status");
+        if ($filter==="All status")
+        {
+            return redirect()->route('service.index');
+        }else{
+            $quaries = Service::where('is_approve',$filter )->where('user_id', $user_id)->with('user')->orderBy('created_at', 'desc')->get();
+        }
+        return view('pages.user_dashbord', compact('quaries','filter'));
+    }
+
+    public function destroy($id)
+    {
+        Service::findOrFail($id)->delete();
+//        $Article = Article::where('id', $id)->delete();
+        return redirect()->route('service.index');
+    }
+}
---
+++ super-clean/resources/views/pages/create/cars.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.app')
+@section('content')
+    @if (Route::has('login'))
+        @auth
+            <script>
+                function myFunctionForCar() {
+                    var service_type = document.getElementById("service_type").value;
+
+                    if (service_type === "Sedan") {
+                        var price = 5
+                    } else if (service_type === "4X4") {
+                        var price = 6
+                    } else {
+                        var price = 7
+                    }
+                    document.getElementById("price").value = price;
+                    document.getElementById("price1").innerHTML = price + " JOD";
+                }
+            </script>
+            <div class="container">
+
+                <form method="post" action="{{route('service.store')}}">
+                    @csrf
+                    {{--                service_category--}}
+                    <div class="form-group">
+                        <h1 style="margin-top: 50px ;color: whitesmoke;padding: 10px 0px 10px 0px ;text-align: center;
+                             background-color:#22a6f5; margin-bottom: 20px">CARS CLEANING</h1>
+                        <input type="hidden" name="service_category" value="Cars">
+                    </div>
+                    {{--            service_type--------------}}
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label" for="service_type">Service Type</label>
+                        <div class="col-10">
+                            <select onchange="myFunctionForCar()" name="service_type" class="form-control"
+                                    id="service_type" required>
+                                <option value="Sedan">Sedan</option>
+                                <option value="4X4">4X4</option>
+                                <option value="Van">Van</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {{--            address         --}}
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label" for="address">City</label>
+                        <div class="col-10">
+                            <select name="city" type="text" class="form-control" >
+                                <option> Amman </option>
+                                <option> Zarqa </option>
+                                <option> Irbid </option>
+                                <option> Almafraq </option>
+                                <option> Albalqa' </option>
+                                <option> Jarash </option>
+                                <option> Ajloun </option>
+                                <option> Madaba </option>
+                                <option> Alkarak </option>
+                                <option> Ma'an </option>
+                                <option> Tafila </option>
+                                <option> Aqaba </option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label" for="address">Address</label>
+                        <div class="col-10">
+                            <input name="address" type="text" class="form-control" id="address"
+                                   placeholder="address"
+                                   required>
+                        </div>
+                    </div>
+                    {{--            date            --}}
+                    <div class="form-group row">
+                        <label for="date" class="col-2 col-form-label">Date and Time</label>
+                        <div class="col-10">
+                            <input name="date" class="form-control" type="datetime-local" id="date">
+                        </div>
+                    </div>
+
+                    <input type="hidden" name="no_of_hours" value=1>
+                    <input type="hidden" name="no_of_workers" value=1>
+
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label" for="price">Price </label>
+                        <div class="col-10">
+                            <input name="price" value="5" type="number" class="form-control" id="price"
+                                   placeholder="price" readonly>
+{{--                            <input name="price" type="hidden" class="form-control" id="price" placeholder="price">--}}
+                        </div>
+                    </div>
+
+                    {{--            description--}}
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label" for="description">Notes</label>
+                        <div class="col-10">
+                    <textarea rows="3" name="description" type="text" class="form-control" id="description"
+                              placeholder="If you have any notes please tell us... "></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary float-right">Submit</button>
+                </form>
+            </div>
+
+
+        @else
+            <div class="container ">
+                <h1 style="text-align: center;">Please Login First</h1>
+            </div>
+            <script>
+                    setTimeout(function(){ location.replace("/login") }, 2000);
+            </script>
+        @endauth
+    @endif
+
+@endsection
+
+
